9638a35a6d81c854cddb1d44676f52df998459f5
MC <neeraj.mohan@capgemini.com>
2022-03-14 11:43:33 +0530

    sample java program

---
+++ src/sample/Author.java
@@ -0,0 +1,37 @@
+package sample;
+
+public class Author {
+	  
+	   private String name;
+	   private String email;
+	   private char gender;  
+	 
+	   /** Constructs a Author instance with the given inputs */
+	   public Author(String name, String email, char gender) {
+	      this.name = name;
+	      this.email = email;
+	      this.gender = gender;
+	   }
+	 
+	  
+	   public String getName() {
+	      return name;
+	   }
+	   
+	   public char getGender() {
+	      return gender;
+	   }
+	
+	   public String getEmail() {
+	      return email;
+	   }
+	  
+	   public void setEmail(String email) {
+	      this.email = email;
+	   }
+	 
+	
+	   public String toString() {
+	      return name + " (" + gender + ") at " + email;
+	   }
+	}
